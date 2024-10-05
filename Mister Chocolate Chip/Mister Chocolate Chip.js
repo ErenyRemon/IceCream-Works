e5890b67386e885f4661098f666f869a0546dfd9
ErenyRemon <161506319+ErenyRemon@users.noreply.github.com>
2024-10-05 18:51:39 +0300

    Add files via upload

---
+++ Mister Chocolate Chip/Mister Chocolate Chip.js
@@ -0,0 +1,58 @@
+let counter = 0;
+
+function increase() {
+    counter++;
+    document.getElementById("counter").value = counter;
+}
+
+
+function decrease() {
+    if(counter > 0) { 
+        counter--;
+        document.getElementById("counter").value = counter;
+    }
+}
+
+
+
+
+function changeImage(element) {
+    
+    let mainImage = document.getElementById('mainImage');
+    
+   
+    mainImage.src = element.src;
+
+   
+    mainImage.style.width = '500px';
+    mainImage.style.height = '500px';
+}
+
+
+
+function nohidden() {
+    var content = document.querySelector('.materials-content');
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+// function toggleVisibility() {
+//     var content = document.querySelector('.careInstructionsContent');
+//     if (content.style.display === "none" || content.style.display === "") {
+//         content.style.display = "block";
+//     } else {
+//         content.style.display = "none";
+//     }
+// }
+
+function nonhide(){
+    var content = document.querySelector('.careInstructionsContent')
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
